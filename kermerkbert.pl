2996350592bc450569a2f1894c343be2c7513863
Andrew P Goetz <goetza@rita.cat.pdx.edu>
2013-03-18 00:32:26 -0700

    started work on bot. Seems like I have everything we will need though...

--- kermerkbert.pl
+++ kermerkbert.pl
@@ -1,11 +1,38 @@
 #!/usr/bin/env perl
 use REST::Client;
-use Bot::BasicBot;
 use JSON;
+package Bot;
+use base qw(Bot::BasicBot);
 use strict;
 use warnings;
 
+sub connected {
+    my $self = shift;
+    my $args = $self->{args};
+    $self->say(who => "nickserv", channel => "msg", body=> "identify ".$args->{passwd});
+    $self->log( "am now connected!\n");
+
+}
+
+sub said {
+    my $self = shift;
+    my $message = shift;
+    if($message->{address} and $message->{who} !~ /nickserv/i) {
+    print "someone tried to talk to us: ".$message->{who}."\n";
+    $self->emote(channel => $self->{args}->{channel}, body => "rages: shut up!");
+    }
+}
+
+sub tick {
+    print "tick called \n";
+    return 5;
+}
+
 sub help {
+    return "Sorry, kermerkbert has trouble herlping perple.";
+}
+package main;
+sub printhelp {
     my $documentation = <<EOF;
 Usage: $0 server channel nick passwd imgursecret	
 EOF
@@ -13,7 +40,7 @@ print $documentation;
     exit 0;
 }
 sub parseargs {
-    help unless (scalar @ARGV == 5);
+    printhelp unless (scalar @ARGV == 5);
     my %arghash = ("server" => $ARGV[0], 
 		   "channel" => $ARGV[1], 
 		   "nick" => $ARGV[2],
@@ -21,5 +48,12 @@ sub parseargs {
 		   "imgursecret" => $ARGV[4]);
     return \%arghash;
 }
-
 my $args = parseargs;
+
+my $bot = Bot->new(channels => [$args->{channel}],
+			     server => $args->{server},
+			     nick => $args->{nick},
+			     name => $args->{nick},
+			     ssl => 1,
+			     port => 6697,
+			     args => $args)->run();
